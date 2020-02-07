005f8b585683dc7fe0c70e9cdd275f59f7c8c7eb
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2020-02-07 23:10:38 +0100

    Fixing setup py

--- frameworks/genens/setup.sh
+++ frameworks/genens/setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 HERE=$(dirname "$0")
 . $HERE/../shared/setup.sh
+PIP install --no-cache-dir -U https://files.pythonhosted.org/packages/16/ae/4bab52c593cad635bcb3e09a9586a047a515fd053188ed7915bf0b5b1482/genens-0.1.8-py3-none-any.whl
 PIP install --no-cache-dir -r $HERE/requirements.txt
