6e20243abc690aa56e67f9de2a1785373f2270f2
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2020-07-25 22:53:06 +0200

    Genens integration with new amlb version

--- frameworks/genens/exec.py
+++ frameworks/genens/exec.py
@@ -2,12 +2,10 @@ import logging
 import os
 import numpy as np
 import pickle
-import pprint
 import random
 import sys
 import tempfile as tmp
 
-from genens.config.clf_stacking import clf_config
 from genens.render.graph import create_graph
 from genens.render.plot import export_plot
 from genens.workflow.evaluate import SampleCrossValEvaluator, CrossValEvaluator
@@ -20,14 +18,9 @@ os.environ['OPENBLAS_NUM_THREADS'] = '1'
 os.environ['MKL_NUM_THREADS'] = '1'
 from genens import GenensClassifier, GenensRegressor
 
-from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, log_loss
-from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_squared_log_error, r2_score, get_scorer
+from sklearn.metrics import get_scorer
 
-from amlb.benchmark import TaskConfig
-from amlb.data import Dataset
-from amlb.datautils import Encoder, impute
-from amlb.results import save_predictions_to_file
-from amlb.utils import Timer, touch
+from frameworks.shared.callee import call_run, result, output_subdir, utils
 
 from typing import Union
 
@@ -35,7 +28,7 @@ from typing import Union
 log = logging.getLogger(__name__)
 
 
-def run(dataset: Dataset, config: TaskConfig):
+def run(dataset, config):
     log.info("\n**** genens ****\n")
 
     is_classification = config.type == 'classification'
@@ -45,28 +38,27 @@ def run(dataset: Dataset, config: TaskConfig):
         return None
 
     # Mapping of benchmark metrics to TPOT metrics
-    metrics_mapping = dict(
-        acc=get_scorer('accuracy'),
-        auc=get_scorer('roc_auc'),
-        f1=get_scorer('f1'),
-        logloss=get_scorer('neg_log_loss'),
-        mae=get_scorer('neg_mean_absolute_error'),
-        mse=get_scorer('neg_mean_squared_error'),
-        msle=get_scorer('neg_mean_squared_log_error'),
-        r2=get_scorer('r2')
-    )
+    metrics_mapping = {
+        'acc': get_scorer('accuracy'),
+        'auc': get_scorer('roc_auc'),
+        'f1': get_scorer('f1'),
+        'logloss': get_scorer('neg_log_loss'),
+        'mae': get_scorer('neg_mean_absolute_error'),
+        'mse': get_scorer('neg_mean_squared_error'),
+        'msle': get_scorer('neg_mean_squared_log_error'),
+        'r2': get_scorer('r2')
+    }
     scoring_metric = metrics_mapping[config.metric] if config.metric in metrics_mapping else None
     if scoring_metric is None:
         raise ValueError("Performance metric {} not supported.".format(config.metric))
 
-    X_train, X_test = impute(dataset.train.X_enc, dataset.test.X_enc)
+    X_train, X_test = dataset.train.X_enc, dataset.test.X_enc
     y_train, y_test = dataset.train.y_enc, dataset.test.y_enc
 
     training_params = {k: v for k, v in config.framework_params.items() if not k.startswith('_')}
     n_jobs = config.framework_params.get('_n_jobs', config.cores)  # useful to disable multicore, regardless of the dataset config
 
     sample_size = config.framework_params.get('_sample_size', None)
-    # sample_size = _heuristic_sample_size(X_train.shape[0], X_train.shape[1])
     if sample_size is not None:
         evaluator = SampleCrossValEvaluator(sample_size=sample_size, per_gen=True, cv_k=5)
     else:
@@ -80,6 +72,14 @@ def run(dataset: Dataset, config: TaskConfig):
     runtime_s = config.max_runtime_seconds
     if runtime_s >= 600:
         runtime_s -= 5 * 60  # avoid premature process termination
+    elif runtime_s > 10:
+        runtime_s -= 5
+
+    if not config.framework_params.get('disable_logging', True):
+        log_path = os.path.join(output_subdir('logs', config), 'evo_log_file.txt')
+    else:
+        log_path = None
+
     print(f"Setting time limit to {runtime_s} seconds.")
 
     log.info('Running genens with a maximum time of %ss on %s cores, optimizing %s.',
@@ -92,21 +92,14 @@ def run(dataset: Dataset, config: TaskConfig):
 
     print(f'Training params: {training_params}')
 
-    node_config = clf_config()
-    node_config.group_weights['ensemble'] = config.framework_params.get('_ens_weight',
-                                                                        node_config.group_weights['ensemble'])
-
-    #log_path = config.output_dir + f'/genens_log_{config.name}.txt'
     estimator = GenensClassifier if is_classification else GenensRegressor
     genens_est = estimator(n_jobs=n_jobs,
-                           config=node_config,
                            max_evo_seconds=runtime_s,
                            scorer=scoring_metric,
-                           #log_path=log_path,
-                           # random_state=config.seed,
+                           log_path=log_path,
                            **training_params)
 
-    with Timer() as training:
+    with utils.Timer() as training:
         genens_est.fit(X_train, y_train)
 
     log.info('Predicting on the test set.')
@@ -120,50 +113,17 @@ def run(dataset: Dataset, config: TaskConfig):
         probabilities = best_pipe.predict_proba(X_test) if is_classification else None
     except AttributeError:
         target_values_enc = dataset.target.label_encoder.transform(dataset.target.values)
-        probabilities = Encoder('one-hot', target=False, encoded_type=float).fit(target_values_enc).transform(predictions)
-
-    save_predictions_to_file(dataset=dataset,
-                             output_file=config.output_predictions_file,
-                             probabilities=probabilities,
-                             predictions=predictions,
-                             truth=y_test,
-                             target_is_encoded=is_classification)
+        probabilities = utils.Encoder('one-hot', target=False, encoded_type=float).fit(target_values_enc).transform(predictions)
 
     save_artifacts(genens_est, config)
 
-    # TODO
-    return dict(
-        models_count=len(genens_est.get_best_pipelines()),
-        training_duration=training.duration
-    )
-
-
-def _heuristic_sample_size(n_rows, n_cols):
-    size = n_rows * n_cols
-
-    # 'small' datasets
-    if size < 10000:
-        return None
-
-    # 'medium' datasets
-    if n_rows < 50000 and n_cols < 10:
-        return 0.5
-
-    if (n_rows < 25000 and n_cols < 100) or n_cols < 30:
-        return 0.25
-
-    # 'large' datasets
-    if (n_rows < 25000 and n_cols < 5000) or n_cols < 100:
-        return 0.1
-
-    # 'very large' datasets
-    return 0.05
-
-
-def make_subdir(name, config):
-    subdir = os.path.join(config.output_dir, name, config.name, str(config.fold))
-    touch(subdir, as_dir=True)
-    return subdir
+    return result(output_file=config.output_predictions_file,
+                  predictions=predictions,
+                  truth=y_test,
+                  probabilities=probabilities,
+                  target_is_encoded=is_classification,
+                  models_count=len(genens_est.get_best_pipelines()),
+                  training_duration=training.duration)
 
 
 def save_artifacts(estimator: Union[GenensClassifier, GenensRegressor], config):
@@ -171,7 +131,7 @@ def save_artifacts(estimator: Union[GenensClassifier, GenensRegressor], config):
         artifacts = config.framework_params.get('_save_artifacts', False)
 
         if 'pickle_models' in artifacts:
-            models_dir = os.path.join(make_subdir('pickle_models', config))
+            models_dir = os.path.join(output_subdir('pickle_models', config))
 
             # pickle top 3 best pipelines
             for i, pipe in enumerate(estimator.get_best_pipelines()):
@@ -179,7 +139,7 @@ def save_artifacts(estimator: Union[GenensClassifier, GenensRegressor], config):
                     pickle.dump(pipe, pickle_file, pickle.HIGHEST_PROTOCOL)
 
         if 'models' in artifacts:
-            models_dir = os.path.join(make_subdir('models', config))
+            models_dir = os.path.join(output_subdir('models', config))
 
             # top 3 individual fitness values
             with open(models_dir + '/ind-fitness.txt', 'w+') as out_file:
@@ -191,7 +151,7 @@ def save_artifacts(estimator: Union[GenensClassifier, GenensRegressor], config):
                     create_graph(ind, models_dir + '/graph{}.png'.format(i))
 
         if 'log' in artifacts:
-            log_dir = os.path.join(make_subdir('logs', config))
+            log_dir = os.path.join(output_subdir('logs', config))
 
             # write logbook string representation to output dir
             with open(log_dir + '/logbook.txt', 'w+') as log_file:
@@ -202,3 +162,6 @@ def save_artifacts(estimator: Union[GenensClassifier, GenensRegressor], config):
 
     except:
         log.debug("Error when saving artifacts.", exc_info=True)
+
+if __name__ == '__main__':
+    call_run(run)
--- frameworks/genens/setup.sh
+++ frameworks/genens/setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 HERE=$(dirname "$0")
-. $HERE/../shared/setup.sh
+. $HERE/../shared/setup.sh $HERE
 
-#SUDO apt-get update -y
+SUDO apt-get update -y
 SUDO apt-get install -y graphviz libgraphviz-dev
 
 PIP install --upgrade pip
-PIP install --no-cache-dir -U https://files.pythonhosted.org/packages/fb/43/d1d84ebc2ad591b15d91754c3213a32a9d81427f97763b3db239b03f54b2/genens-0.1.9-py3-none-any.whl
+PIP install --no-cache-dir -U https://files.pythonhosted.org/packages/97/9c/e66fcda22a421761cd4792c4133757708d8352a28d8770652b10271e8829/genens-0.1.15-py3-none-any.whl
 
 PIP install --no-cache-dir -r $HERE/requirements.txt
