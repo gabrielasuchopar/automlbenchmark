0067607b772c9c6a50a268c710a711e680eb8b1c
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2020-07-27 22:42:52 +0200

    Better genens setup, model png as a separate artifact.

--- frameworks/genens/setup.sh
+++ frameworks/genens/setup.sh
@@ -6,6 +6,6 @@ SUDO apt-get update -y
 SUDO apt-get install -y graphviz libgraphviz-dev
 
 PIP install --upgrade pip
-PIP install --no-cache-dir -U https://files.pythonhosted.org/packages/97/9c/e66fcda22a421761cd4792c4133757708d8352a28d8770652b10271e8829/genens-0.1.15-py3-none-any.whl
+PIP install --no-cache-dir genens==0.1.15
 
 PIP install --no-cache-dir -r $HERE/requirements.txt
